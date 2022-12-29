2a549c78bbd5cdf22b31de07e4873f65fba1f715
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-12-29 11:40:51 +0530

    Added addRecord.js

---
+++ core/addRecord.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import {Pressable, Text, TextInput, View} from 'react-native';
+import styles from './styles'
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+class AddRecord extends React.Component {
+    state = {
+        trackers: []
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        )
+    }
+}
